3f9ec65e2a8992232bc4cd42d9cdd394ea5ae625
Vladislav Pavlyuk <vladpavliuk@gmail.com>
2024-09-19 08:13:31 +0200

    All tests renamed

--- src/test/java/brainacad/ShapeTest.java
+++ src/test/java/brainacad/ShapeTest.java
@@ -8,25 +8,25 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ShapeTest {
 
     @Test
-    public void ShapeTest_should_return_25_if_called() {
+    public void triangleTest_should_return_25_if_called() {
         Triangle triangle = new Triangle(10, 5);
         assertEquals(25, triangle.calculateArea());
     }
 
     @Test
-    public void ShapeTest_should_return_50_if_called() {
+    public void rectangleTest_should_return_50_if_called() {
         Rectangle rectangle = new Rectangle(10, 5);
         assertEquals(50, rectangle.calculateArea());
     }
 
     @Test
-    public void ShapeTest_should_return_36_if_called() {
+    public void squareTest_should_return_36_if_called() {
         Square square = new Square(6);
         assertEquals(36, square.calculateArea());
     }
 
     @Test
-    public void ShapeTest_should_return_10_if_called() {
+    public void rhombusTest_should_return_10_if_called() {
         Rhombus rhombus = new Rhombus(4, 5);
         assertEquals(10, rhombus.calculateArea());
     }
--- src/test/java/brainacad/WeightConverterTest.java
+++ src/test/java/brainacad/WeightConverterTest.java
@@ -9,27 +9,27 @@ public class WeightConverterTest {
     private final WeightConverter converter = new WeightConverter();
 
     @Test
-    public void WeightConverterTest_should_return_1_000_000_if_called() {
+    public void toMilligramsConverterTest_should_return_1_000_000_if_called() {
         assertEquals(1_000_000, converter.toMilligrams(1), 0.001);
     }
 
     @Test
-    public void WeightConverterTest_should_return_1_000_if_called() {
+    public void toGramsConverterTest_should_return_1_000_if_called() {
         assertEquals(1_000, converter.toGrams(1), 0.001);
     }
 
     @Test
-    public void WeightConverterTest_should_return_1_if_called() {
+    public void toKilogramsConverterTest_should_return_1_if_called() {
         assertEquals(1, converter.toKilograms(1), 0.001);
     }
 
     @Test
-    public void WeightConverterTest_should_return_01_if_called() {
+    public void toCentnersConverterTest_should_return_01_if_called() {
         assertEquals(0.01, converter.toCentners(1), 0.001);
     }
 
     @Test
-    public void WeightConverterTest_should_return_001_if_called() {
+    public void toTonsConverterTest_should_return_001_if_called() {
 
         assertEquals(0.001, converter.toTons(1), 0.001);
     }
