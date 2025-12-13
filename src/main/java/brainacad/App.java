05da157db889cc263be83e88c3f3ab6641608247
Vlad Pavliuk <vladpavliuk@gmail.com>
2025-12-13 12:33:59 +0100

    Enhance App functionality with additional method calls and improve StringUtils usage - Added method calls for retrieving rectangle dimensions, currency exchange rates, and supported currencies in App.java. - Expanded StringUtils usage with methods for counting vowels, consonants, and word occurrences in a given string.

--- src/main/java/brainacad/App.java
+++ src/main/java/brainacad/App.java
@@ -15,6 +15,8 @@ public class App
         /// Задание 1
         Rectangle rectangle = new Rectangle();
         rectangle.calculateArea();
+        rectangle.getLength();
+        rectangle.getWidth();
 
         Rhombus rhombus = new Rhombus();
         rhombus.calculateArea();
@@ -26,13 +28,19 @@ public class App
         triangle.calculateArea();
 
         //Задание 2
-
         CurrencyConverter currencyConverter = new CurrencyConverter();
         currencyConverter.convert("EUR","USD",1);
+        currencyConverter.getExchangeRate("EUR");
+        currencyConverter.getSupportedCurrencies();
+        currencyConverter.isCurrencySupported("EUR");
 
         //Задание 3
         StringUtils stringUtils = new StringUtils();
         stringUtils.isPalindrome("А роза упала на лапу Азора");
+        stringUtils.countVowels("А роза упала на лапу Азора");
+        stringUtils.countConsonants("А роза упала на лапу Азора");  
+        stringUtils.countWordOccurrences("А роза упала на лапу Азора", "Азора");
+
 
         //Задание 4
         LengthConverter lengthConverter = new LengthConverter();
