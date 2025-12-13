8fcc3dd48781d1d0e5d109a4a9c5346522763542
Vlad Pavliuk <vladpavliuk@gmail.com>
2025-12-13 12:47:14 +0100

    Enhance App.java with additional conversion methods for LengthConverter and WeightConverter. Added methods for converting between various units, improving functionality and usability.

--- src/main/java/brainacad/App.java
+++ src/main/java/brainacad/App.java
@@ -41,13 +41,45 @@ public class App
         stringUtils.countVowels("А роза упала на лапу Азора");
         stringUtils.countConsonants("А роза упала на лапу Азора");  
         stringUtils.countWordOccurrences("А роза упала на лапу Азора", "Азора");
-        
+
+
         //Задание 4
         LengthConverter lengthConverter = new LengthConverter();
         lengthConverter.toCentimeters(100);
+        lengthConverter.toMillimeters(100);
+        lengthConverter.toDecimeters(100);
+        lengthConverter.toMeters(100);
+        lengthConverter.toKilometers(100);
+        lengthConverter.convert("mm", "cm", 100);
+        lengthConverter.convert("cm", "mm", 100);
+        lengthConverter.convert("dm", "mm", 100);
+        lengthConverter.convert("m", "mm", 100);
+        lengthConverter.convert("km", "mm", 100);
+        lengthConverter.convert("mm", "dm", 100);
+        lengthConverter.convert("cm", "dm", 100);
+        lengthConverter.convert("m", "dm", 100);
+        lengthConverter.convert("km", "dm", 100);
+        lengthConverter.convert("mm", "m", 100);
+        lengthConverter.convert("cm", "m", 100);
+        lengthConverter.convert("dm", "m", 100);
+        lengthConverter.convert("km", "m", 100);
+        lengthConverter.convert("mm", "km", 100);
+        lengthConverter.convert("cm", "km", 100);
+        lengthConverter.convert("dm", "km", 100);
+        lengthConverter.convert("m", "km", 100);
 
         //Задание 5
         WeightConverter weightConverter = new WeightConverter();
         weightConverter.toCentners(100);
+        weightConverter.toGrams(100);
+        weightConverter.toKilograms(100);
+        weightConverter.toMilligrams(100);
+        weightConverter.convert("mg", "g", 100);
+        weightConverter.convert("g", "mg", 100);
+        weightConverter.convert("kg", "mg", 100);
+        weightConverter.convert("kg", "g", 100);
+        weightConverter.convert("kg", "mg", 100);
+        weightConverter.convert("mg", "kg", 100);
+        weightConverter.convert("g", "kg", 100);
     }
 }
